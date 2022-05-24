a762ba3ccb59de8ca2e219cde3d4e688b0d208be
Kerlin <lopeskerlin@gmail.com>
2022-05-24 16:27:55 -0400

    theme changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,58 +1,30 @@
 import s from './Footer.module.scss';
 import { AiFillGithub } from 'react-icons/ai';
-import {
-  FaFacebookSquare,
-  FaLinkedinIn,
-  FaTelegramPlane,
-} from 'react-icons/fa';
+import { FaLinkedinIn } from 'react-icons/fa';
 
 const Footer = () => {
-  let date = new Date();
-  let year = date.getFullYear();
-
   return (
     <div className={s.footer}>
       <div className={s.container}>
         <div className={s.copyright}>
-          <h3>Designed and Developed by Snaichuk Volodymyr</h3>
-        </div>
-
-        <div className={s.copyright}>
-          <h3>Copyright © {year} SV.dev</h3>
+          <h3>Developed by KERLIN</h3>
         </div>
 
         <div className={s.body}>
           <ul className={s.socialIcons}>
             <li>
               <a
-                href="https://github.com/vsnaichuk"
+                href="https://github.com/kerlinlopes/kerlinlopes"
                 target="_blank"
                 rel="noreferrer"
               >
                 <AiFillGithub />
               </a>
             </li>
+
             <li>
               <a
-                href="https://www.facebook.com/Snaychuk"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaFacebookSquare />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://t.me/snaichuk_v"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <FaTelegramPlane />
-              </a>
-            </li>
-            <li>
-              <a
-                href="https://www.linkedin.com/in/volodymyr-snaichuk-74a389124/"
+                href="https://www.linkedin.com/in/kerlinlopes/"
                 target="_blank"
                 rel="noreferrer"
               >
